7eb6023e3775fff56216acfaf7cea7cac5576a73
Parth Patel <parthpatelgee@gmail.com>
2017-02-15 21:05:47 +0000

    Added posts since last commit. The website is now fully functional

--- express_server.js
+++ express_server.js
@@ -17,17 +17,17 @@ function generateRandomString() {
   }
   return randomAlphas;
 }
-generateRandomString();
+//generateRandomString();
 
 var urlDatabase = {
-"b2xVn2": "http://www.lighthouselabs.ca",
-"9sm5xK": "http://www.google.com",
-"8qp9PO": "http://www.youtube.com"
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+  "8qp9PO": "http://www.youtube.com"
 };
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
-});
+//app.get("/", (req, res) => {
+  //res.end("Hello!");
+//});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -39,12 +39,12 @@ app.get("/urls", (req, res) => {
 })
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] }
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] }
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars)
 })
 
@@ -55,15 +55,29 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let o = generateRandomString();
+  console.log(o);
+  let longURL = req.body.longURL;
+  console.log(longURL);
+  urlDatabase[o] = longURL;
+  console.log(urlDatabase);
+  res.render("urls_show", {'longURL':longURL, 'shortURL':o});
+  //console.log(req.body);  // debug statement to see POST parameters
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars)
+});
 
-//app.get("/hello", (req, res) => {
-//res.end("<html><body>Hello <b>World</b></body></html>\n");
-//});
+app.post("/urls/:id/update", (req, res) => {
+  let longURL = req.body.longURL;
+  urlDatabase[req.params.id] = longURL;
+  res.redirect("/urls")
+})
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
