27cfc82ae40ccbc32e2c5e0819b7776f4ca48ff6
Parth Patel <parthpatelgee@gmail.com>
2017-02-16 20:36:49 +0000

    registration WITH the header works

--- express_server.js
+++ express_server.js
@@ -21,6 +21,29 @@ function generateRandomString() {
   }
   return randomAlphas;
 }
+
+var users = {
+  "user@example.com": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2@example.com": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+  "user3@example.com": {
+    id: "user3RandomID",
+    email: "user3@example.com",
+    password: "tatti"
+  },
+  "user4@example.com": {
+    id: "user4RandomID",
+    email: "user4@example.com",
+    password: "kamine"
+  }
+}
 //generateRandomString();
 
 var urlDatabase = {
@@ -62,13 +85,21 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.get("/register", (req, res) =>{
+  res.render("urls_register", {message: null})
+})
+
+app.get("/login", (req, res) =>{
+  res.render("urls_login")
+})
+
 app.post("/urls", (req, res) => {
   let o = generateRandomString();
-  console.log(o);
+  //console.log(o);
   let longURL = req.body.longURL;
-  console.log(longURL);
+  //console.log(longURL);
   urlDatabase[o] = longURL;
-  console.log(urlDatabase);
+  //console.log(urlDatabase);
   res.redirect("/urls/" + o);
   //console.log(req.body);  // debug statement to see POST parameters
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
@@ -88,14 +119,48 @@ app.post("/urls/:id/update", (req, res) => {
 
 app.post("/login", (req, res) =>{
   res.cookie('username', req.body.username);
+  console.log('this', req.body.username);
+  if(req.body.username){
+    res.redirect("/urls/new")
+  }
+  else{
+    res.redirect("/register")
+  }
   //console.log(req.body.username + "****************************THIS ONE********************");
-  res.redirect("urls/new")
+
 
 })
 
 app.post("/logout", (req, res) => {
 res.clearCookie('username');
 res.redirect("/urls/new")
+})
+
+app.post("/register", (req, res) => {
+  let userID = generateRandomString();
+  let userEMAIL = req.body.email;
+  let userPASSWORD = req.body.password;
+  if(userEMAIL === '' || userPASSWORD === ''){
+    res.statusCode = 400;
+    let message = {message: "email and/or password can't be empty. D'uh."}
+    res.render("urls_register", message);
+  }
+  // else{
+  //   if(userEMAIL){
+  //     res.statusCode = 400;
+  //     res.render("/register")
+  //   }
+  //   res.redirect("/urls/new")
+  // }
+  users[userEMAIL] = {
+    id: userID,
+    email: userEMAIL,
+    password: userPASSWORD
+  }
+  res.cookie("username", userEMAIL);
+  console.log(users);
+
+  res.redirect("/urls/new")
 })
 
 app.listen(PORT, '0.0.0.0', () => {
